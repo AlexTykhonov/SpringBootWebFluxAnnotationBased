f9ea59aa81d4d96d92afc156ab9ebe6ccf251c06
AlexTykhonov <alexey.tykhonov@gmail.com>
2019-06-10 22:41:51 +0300

    + cross origin

--- src/main/java/com/javasampleapproach/webflux/controller/RestControllerAPIs.java
+++ src/main/java/com/javasampleapproach/webflux/controller/RestControllerAPIs.java
@@ -8,20 +8,14 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.javasampleapproach.webflux.model.Customer;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/api/customer")
 public class RestControllerAPIs {
